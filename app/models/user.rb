06059daefe788e779035ee20fdcbba1c20e7000f
treva556 <marktreva92@gmail.com>
2023-09-11 23:10:18 +0300

    user specifics

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ApplicationRecord
+
+    enum role: [:client, :seller, :admin]
+  
+    # Common user fields (e.g., name, email, password)
+  
+    # Additional attributes or methods for distinguishing user roles:
+    def admin?
+      role == 'admin'
+    end
+  
+    def seller?
+      role == 'seller'
+    end
 end
