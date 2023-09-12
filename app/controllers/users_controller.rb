734758d7e517ffe9768405233038a12c61e6cdb9
treva556 <marktreva92@gmail.com>
2023-09-12 19:57:50 +0300

    user specifics

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,4 @@
 class UsersController < ApplicationController
     # Implement actions for user registration, login, profile management, and admin functionalities.
+    
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,12 @@ class User < ApplicationRecord
     enum role: [:client, :seller, :admin]
   
     # Common user fields (e.g., name, email, password)
-  
+    validates :name, presence: true
+    validates :email, presence: true,
+    validates :password, presence: true
+    validates :role, presence: true
+
+
     # Additional attributes or methods for distinguishing user roles:
     def admin?
       role == 'admin'
